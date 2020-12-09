b04c6d4810d2cd0afd00bf2c670df51533a4f44e
Ricky Justiniano <justiniano.ricky@gmail.com>
2020-12-09 16:24:00 -0700

    delete button

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,11 +1,12 @@
 const savedNotes = require("../db/db.json");
 const fs = require("fs");
+const path = require("path");
 const uuid = require("uuid");
 
 module.exports = function (app) {
   // GET /api/notes - Should read the db.json file and return all saved notes as JSON.
   app.get("/api/notes", function (req, res) {
-    res.json(savedNotes);
+    res.sendFile(path.join(__dirname, "../db/db.json"))
   });
 
   // POST /api/notes - Should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
